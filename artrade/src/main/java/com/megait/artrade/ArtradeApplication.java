67363f848e00336220912d9a29279e2525ceaec6
Sera Lee <ssellu.lee@gmail.com>
2021-08-12 17:01:03 +0900

    프로젝트 생성

---
+++ artrade/src/main/java/com/megait/artrade/ArtradeApplication.java
@@ -0,0 +1,13 @@
+package com.megait.artrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtradeApplication.class, args);
+	}
+
+}
---
+++ artrade/src/test/java/com/megait/artrade/ArtradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megait.artrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
