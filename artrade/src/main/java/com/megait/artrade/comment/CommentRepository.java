55a7e395f00f5dad9c51ea34e02ff8a656be5b0c
Park Yeon Jae <heroman97@naver.com>
2021-08-27 03:16:44 +0900

    댓글 로직 진행중-(1)

--- artrade/src/main/java/com/megait/artrade/comment/CommentRepository.java
+++ artrade/src/main/java/com/megait/artrade/comment/CommentRepository.java
@@ -1,9 +1,8 @@
 package com.megait.artrade.comment;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository {
-
-//    Comment findBy
+public interface CommentRepository extends JpaRepository<> {
 }
--- artrade/src/main/java/com/megait/artrade/comment/CommentVo.java
+++ artrade/src/main/java/com/megait/artrade/comment/CommentVo.java
@@ -1,4 +1,17 @@
 package com.megait.artrade.comment;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentVo {
+
+    private String comment;
 }
