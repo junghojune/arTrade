4ba8107ca29bc71f5c6535f977780d4c4d36f346
hek316 <hek316@naver.com>
2021-08-27 04:54:18 +0900

    ajax csrf토큰 추가

--- artrade/src/main/resources/static/js/work/regauction.js
+++ artrade/src/main/resources/static/js/work/regauction.js
@@ -8,12 +8,19 @@ $(document).ready(function (){
         };
         console.log(data); // 자바스크립트 오브젝트
         console.log(JSON.stringify(data));
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
         $.ajax({
             type: 'POST',
             url: '/auction/registration', // 요청보낼 주소
             data: JSON.stringify(data),
+            beforeSend : function(xhr)
+            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+                xhr.setRequestHeader(header, token);
+            },
             contentType: "application/json; charset=utf-8",
-            dataType: 'json',    // 응답의 content-type (json 타입으로 받겠다!)
+            dataType: 'json',   // 응답의 content-type (json 타입으로 받겠다!)
+
             success: function (result){ // 요청을 성공적으로 했다면
                 $('.modal-body').html(result.message);
                 $('#resultbtn').html(result.guide);
@@ -29,6 +36,9 @@ $(document).ready(function (){
     });
 });
 
+
+
+
 // 모달 안 버튼
 $(document).ready(function (){
     $("#resultbtn").click(function () {
@@ -41,7 +51,9 @@ $(document).ready(function (){
                     location.href="/login";
                 }else if (result.status === "notCheckToken"){
                   //   location.href="/"; // TODO NFT 발급 주소로 바꿔주기
-                }else{
+                }else if(result.status ==='ok'){
+                    location.href="/openMarket";
+                }else {
                     location.href="/";
                 }
             }
@@ -53,6 +65,8 @@ $(document).ready(function (){
 
 // 업로드 버튼 클릭
 $(document).ready(function (){
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
     $("#upload").click(function () {
         let data = {
             id: $("#workSelect").val() ,
@@ -67,6 +81,10 @@ $(document).ready(function (){
             type: 'POST',
             url: '/auction/upload', // 요청보낼 주소
             data: JSON.stringify(data),
+            beforeSend : function(xhr)
+            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+                xhr.setRequestHeader(header, token);
+            },
             contentType: "application/json; charset=utf-8",
             dataType: 'json',    // 응답의 content-type (json 타입으로 받겠다!)
             success: function (result){ // 요청을 성공적으로 했다면
@@ -79,5 +97,7 @@ $(document).ready(function (){
         });
     });
 });
+
+
 
 // TODO 업로드 완료시 확인 클릭하면 mypage 이동
